62c26000a76175c861df7c270043570b7b5c9767
Sam Daigle <110954530+samdaigle@users.noreply.github.com>
2025-10-17 13:19:09 -0400

    add reset button

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const main = document.querySelector("body");
 const script = document.querySelector("script");
 const container = document.createElement("div");
+const header = document.createElement("div");
+const btn = document.createElement("button");
 
+header.id = "header";
 container.id = "container";
+
 main.insertBefore(container, script);
+main.insertBefore(header, container);
+
+
+header.appendChild(btn);
+btn.textContent = "Reset";
+
 
 
 for (let i = 0; i < 256; i++) {
