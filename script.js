162037f524715729165b960a1b6cd6d6916e922e
Sam Daigle <110954530+samdaigle@users.noreply.github.com>
2025-10-17 14:04:23 -0400

    add functions to reset grid

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const TOTAL_WIDTH = 480;
+const START_NUM_SQUARES = 16;
+
+
 const main = document.querySelector("body");
 const script = document.querySelector("script");
 const container = document.createElement("div");
@@ -10,22 +14,58 @@ container.id = "container";
 main.insertBefore(container, script);
 main.insertBefore(header, container);
 
-
 header.appendChild(btn);
 btn.textContent = "Reset";
 
 
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement("div");
-    square.className = "square";
-    container.appendChild(square);
+btn.addEventListener("click", () => {
+    let numSquares = prompt("Set the number of squares: ")
+    removeGrid();
+    addGrid(+numSquares);
+    setSquareWidth(+numSquares);
+    setHover();
+})
+
+function setHover() {
+    const square = document.querySelectorAll(".square");
+    square.forEach( (square) => {
+        square.addEventListener("mouseenter", () => {
+            square.style.backgroundColor = "black";
+        })
+    })
 }
 
-const square = document.querySelectorAll(".square");
-
-square.forEach( (square) => {
-    square.addEventListener("mouseenter", () => {
-        square.style.backgroundColor = "black";
+function removeGrid() {
+    const square = document.querySelectorAll(".square");
+    square.forEach((square) => {
+        container.removeChild(square);
     })
-})
+}
+
+function addGrid(numSquares) {
+    const square = document.querySelectorAll(".square");
+    totalSquares = numSquares ** 2;
+    
+    for (let i = 0; i < totalSquares; i++) {
+        const newSquare = document.createElement("div");
+        newSquare.className = "square";
+        container.appendChild(newSquare);
+    }
+}
+
+function setSquareWidth(numSquares) {
+
+    squareWidth = TOTAL_WIDTH / numSquares;
+    const square = document.querySelectorAll(".square");
+    square.forEach((square) => {
+        square.style.width = squareWidth + "px";
+        square.style.height = squareWidth + "px";
+    })
+
+}
+
+
+addGrid(START_NUM_SQUARES);
+setHover();
+
